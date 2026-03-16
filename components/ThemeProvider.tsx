33e0f4abd29a16234cfc8587aec5f4471e4d37d9
Solai Rajan <solai13kamaraj@gmail.com>
2026-03-16 21:59:13 +0530

    Fix: Remove invalid ThemeProviderProps import from next-themes

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import React from 'react'
-import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
 /**
  * Theme Provider wrapper component for next-themes
  * Handles dark/light mode switching across the application
  */
-export function ThemeProvider(props: ThemeProviderProps) {
-  return <NextThemesProvider {...props} />
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return <NextThemesProvider>{children}</NextThemesProvider>
 }
